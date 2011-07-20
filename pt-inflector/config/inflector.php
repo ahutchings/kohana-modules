9a36cb54c12e5b912ebc64984efeaef02ce0327e
xurumelous <xurumelous@gmail.com>
2011-07-20 11:23:17 -0300

    3.1: Adding pt-inflector module

---
+++ pt-inflector/config/inflector.php
@@ -0,0 +1,49 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+
+	'uncountable' => array(
+		'atlas',
+		'lapis',
+		'onibus',
+		'pires',
+		'virus'
+	),
+
+	'irregular' => array(
+		'abdomen' => 'abdomens',
+		'alemao' => 'alemaes',
+		'artesa' => 'artesaos',
+		'as' => 'ases',
+		'bencao' => 'bencaos',
+		'cao' => 'caes',
+		'capelao' => 'capelaes',
+		'capitao' => 'capitaes',
+		'chao' => 'chaos',
+		'charlatao' => 'charlataes',
+		'cidadao' => 'cidadaos',
+		'consul' => 'consules',
+		'cristao' => 'cristaos',
+		'dificil' => 'dificeis',
+		'email' => 'emails',
+		'escrivao' => 'escrivaes',
+		'fossel' => 'fosseis',
+		'germens' => 'germen',
+		'grao' => 'graos',
+		'hifens' => 'hifen',
+		'irmao' => 'irmaos',
+		'liquens' => 'liquen',
+		'mal' => 'males',
+		'mao' => 'maos',
+		'orfao' => 'orfaos',
+		'pais' => 'paises',
+		'pai' => 'pais',
+		'pao' => 'paes',
+		'perfil' => 'perfis',
+		'projetil' => 'projeteis',
+		'reptil' => 'repteis',
+		'sacristao' => 'sacristaes',
+		'sotao' => 'sotaos',
+		'tabeliao' => 'tabeliaes'
+	),
+);
