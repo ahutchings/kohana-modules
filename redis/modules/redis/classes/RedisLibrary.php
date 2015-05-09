b41489f5cdbc461ed4b72ec52bf2ed4e5951d955
Xackery Xtal <xackery@gmail.com>
2015-05-08 17:39:07 -0700

    Removed redis files, added a submodule to phpredis instead.

--- redis/modules/redis/classes/RedisLibrary.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-abstract class RedisLibrary extends Kohana_Redis {}
--- redis/modules/redis/config/session.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-return array(
-	'redis' => array(
-		'group'   => 'default',
-		'lifetime' => 1800,
-	),
-);
